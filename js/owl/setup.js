0069c114b23b2b8988eb92a19e73c98417964d1a
belinhamesmo <isabelleoliveiradesign@gmail.com>
2021-11-02 19:37:14 -0300

    popflix

---
+++ js/owl/setup.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        900:{
+            items:4
+        },
+        1250:{
+            items:5
+        }
+    }
+})
